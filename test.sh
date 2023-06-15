c2d70414ccdd974d34307e8dbe663c49276ee077
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-06-15 10:34:51 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -47,3 +47,55 @@ echo "Enter packages you want installed (Default base base-devel linux linux-fir
 read packages
 
 pacstrap -K /mnt $packages base base-devel linux linux-firmware linux-headers intel-ucode sudo nano dhcpcd gvim
+
+genfstab -U /mnt > /mnt/etc/fstab
+
+arch-chroot /mnt
+
+echo "Enter root password"
+
+passwd
+
+echo "Enter username"
+
+read user
+
+useradd -m $user
+
+echo "Enter user password"
+
+passwd $user
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+
+locale-gen
+
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+export LANG=en_US.UTF-8
+
+echo "Enter Hostname"
+
+read hostname
+
+echo $hostname > /etc/hostname
+
+echo "127.0.0.1      localhost
+::1            localhost
+127.0.1.1      $hostname.localdomain  localhost" >> /etc/hosts
+
+cat /etc/hosts
+
+echo "wait"
+
+read $null
+
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+
+hwclock --systohc
+
+pacman -S grub efibootmgr dosfstools mtools
+
+mkinitcpio -p linux
+
+echo "Now go install grub"
