80d1ec3a34fc73220cbe4478bce56f71ea426f7c
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-06-15 14:45:07 -0400

    Create VMInstall-3.sh

---
+++ VM/VMInstall-3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+git clone https://aur.archlinux.org/yay.git
+
+cd yay
+
+makepkg -si
+
+yay -S picom-git
+
+cd ~/
+
+cp /etc/xdg/picom.conf.example ~/.config/picom/picom.conf
+
+sudo pacman -S i3 xorg xorg-xinit nitrogen dmenu alacritty firefox thunar gvim sddm
+
+cp /usr/share/doc/alacritty/example/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+touch .xinitrc
+
+echo "nitrogen --restore &
+picom &
+exec i3" >> .xinitrc
+
+echo "[[ $(fgconsole 2>/dev/null) == 1 ]] && exec startx -vt1" >> .bash_profile
+
+sudo pacman -S pipewire-jack pipewire-alsa pipewire-pulse qjackctl pavucontrol
