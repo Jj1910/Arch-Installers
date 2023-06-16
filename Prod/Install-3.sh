40a7cffc54bd3c181916b3ce1db784603f46b473
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2023-06-16 10:54:21 -0400

    Update Install-3.sh

--- Prod/Install-3.sh
+++ Prod/Install-3.sh
@@ -14,7 +14,9 @@ echo 'backend = "glx";
 glx-no-stencil = true;
 glx-copy-from-front = false;' >> ~/.config/picom/picom.conf
 
-sudo pacman -Sy nvidia-settings i3 xorg xorg-xinit nitrogen dmenu alacritty firefox thunar sddm ttf-firacode-nerd pipewire-jack pipewire-alsa pipewire-pulse qjackctl pavucontrol pass cifs-utils polybar rofi
+sudo pacman -Sy nvidia-settings i3 xorg xorg-xinit nitrogen alacritty firefox thunar sddm ttf-firacode-nerd pipewire-jack pipewire-alsa pipewire-pulse qjackctl pavucontrol pass cifs-utils polybar rofi
+
+mkdir ~/.config/alacritty
 
 sudo cp /usr/share/doc/alacritty/example/alacritty.yml ~/.config/alacritty/alacritty.yml
 
