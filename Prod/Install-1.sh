caff446478a385ea6937130323d8367aafa16b1d
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2025-04-03 08:14:16 -0400

    Update Install-1.sh

--- Prod/Install-1.sh
+++ Prod/Install-1.sh
@@ -4,7 +4,7 @@ setfont ter-132n
 
 timedatectl set-timezone America/New_York
 
-mdadm --create --verbose --level=0 --metadata=1.2 --raid-devices=2 /dev/md/LINUX /dev/nvme1n1p1 /dev/nvme2n1p1
+#mdadm --create --verbose --level=0 --metadata=1.2 --raid-devices=2 /dev/md/LINUX /dev/nvme1n1p1 /dev/nvme2n1p1
 
 lsblk
 
@@ -28,6 +28,10 @@ echo "Enter Home Partition"
 
 read homepart
 
+echo "Enter EFI Partition"
+
+read efipart
+
 mkfs.ext4 /dev/$rootpart
 
 mkfs.ext4 /dev/$homepart
@@ -42,29 +46,21 @@ mkdir /mnt/home
 
 mount /dev/$homepart /mnt/home
 
+mount /dev/$efipart /mnt/efi
+
 lsblk
 
-echo "Enter Extra Packages to Install (Default: base base-devel linux linux-firmware linux-headers intel-ucode sudo nano dhcpcd mdadm ntfs-3g nvidia gvim git vim openssh)"
+echo "Enter Extra Packages to Install (Default: base base-devel linux linux-firmware linux-headers intel-ucode sudo nano dhcpcd ntfs-3g nvidia gvim git vim openssh cifs-utils)"
 
 read packages
 
-pacstrap -K -i /mnt $packages base base-devel linux linux-firmware linux-headers intel-ucode sudo nano dhcpcd mdadm ntfs-3g nvidia gvim git vim openssh
+pacstrap -K -i /mnt $packages base base-devel linux linux-firmware linux-headers intel-ucode sudo nano dhcpcd ntfs-3g nvidia gvim git vim openssh cifs-utils
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-lsblk
+#mdadm --detail --scan >> /mnt/etc/mdadm.conf
 
-echo "Enter EFI Partition"
-
-read efipart
-
-mkdir /mnt/efi
-
-mount /dev/$efipart /mnt/efi
-
-mdadm --detail --scan >> /mnt/etc/mdadm.conf
-
-echo "Make sure to correct genfstab if RAID is configured"
+#echo "Make sure to correct genfstab if RAID is configured"
 
 sleep 1
 
