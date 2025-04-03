95d0e96982c789edef1055d40248a2ac437ffc30
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2025-04-03 08:20:38 -0400

    Update Install-2.sh

--- Prod/Install-2.sh
+++ Prod/Install-2.sh
@@ -40,11 +40,11 @@ ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 
 hwclock --systohc
 
-pacman -Sy grub efibootmgr dosfstools mtools
+#pacman -Sy grub efibootmgr dosfstools mtools
 
-sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=10/g' /etc/default/grub
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="intel_iommu=on iommu=pt loglevel=4 nvidia_drm.modeset=1"/g' /etc/default/grub
-sed -i 's/#GRUB_DISABLE_LINUX_UUID=true/GRUB_DISABLE_LINUX_UUID=true/g' /etc/default/grub
+#sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=10/g' /etc/default/grub
+#sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="intel_iommu=on iommu=pt loglevel=4 nvidia_drm.modeset=1"/g' /etc/default/grub
+#sed -i 's/#GRUB_DISABLE_LINUX_UUID=true/GRUB_DISABLE_LINUX_UUID=true/g' /etc/default/grub
 
 sed -i 's/MODULES=.*/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g' /etc/mkinitcpio.conf
 sed -i 's/HOOKS=.*/HOOKS=(base udev autodetect modconf keyboard keymap consolefont block mdadm_udev filesystems fsck)/g' /etc/mkinitcpio.conf
@@ -52,14 +52,36 @@ sed -i 's/BINARIES=.*/BINARIES=(/sbin/mdmon)/g' /etc/mkinitcpio.conf
 
 mkinitcpio -p linux
 
-grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB --recheck
+#grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB --recheck
 
-echo "menuentry 'Windows 11' {
-	search --fs-uuid --set=root BA2C-C152
-	chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-}" >> /etc/grub.d/40_custom
+bootctl install
 
-grub-mkconfig -o /boot/grub/grub.cfg
+#echo "menuentry 'Windows 11' {
+#	search --fs-uuid --set=root BA2C-C152
+#	chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+#}" >> /etc/grub.d/40_custom
+
+#grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "default arch.conf
+timeout 0
+console-mode auto" > /boot/loader/loader.conf
+
+echo "Enter EFI Partition UUID (PARTUUID)"
+
+blkid
+
+read $partuuid
+
+echo "title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=PARTUUID=$partuuid rw" > /boot/loader/entries/arch.conf
+
+echo "title Arch Linux Fallback
+linux /vmlinuz-linux
+initrd /initramfs-linux-fallback.img
+options root=PARTUUID=$partuuid rw" > /boot/loader/entries/arch-fallback.conf
 
 systemctl enable dhcpcd systemd-timesyncd
 
