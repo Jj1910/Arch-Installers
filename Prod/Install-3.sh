b0a5b12e5839f1bccc0623cda65cc4f9cedf6ea4
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2025-04-07 06:40:52 -0400

    Update Install-3.sh

--- Prod/Install-3.sh
+++ Prod/Install-3.sh
@@ -57,6 +57,6 @@ sudo pacman -S  pipewire-jack pipewire-alsa pipewire-pulse pass htop ncdu neofet
 sudo pacman -S bluez bluez-utils bluedevil
 
 ## Desktop Packages
-sudo pacman -S sddm plasma-desktop kscreen konsole dolphin plasma-pa kde-gtk-config nvidia-settings firefox qtpass qjackctl sddm-kcm plasma-nm
+sudo pacman -S sddm plasma-desktop kscreen konsole dolphin plasma-pa kde-gtk-config nvidia-settings firefox qtpass qjackctl sddm-kcm plasma-nm plasma-systemmonitor
 
 sudo systemctl enable sddm
