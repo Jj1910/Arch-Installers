4af1d9ee08b3b8ae0daeefc5ca1f0d88e6c1e322
Jj1910 <32131060+Jj1910@users.noreply.github.com>
2025-05-03 16:03:26 -0400

    Update Install-3.sh

--- Prod/Install-3.sh
+++ Prod/Install-3.sh
@@ -60,6 +60,6 @@ sudo pacman -S bluez bluez-utils bluedevil
 sudo pacman -S sddm plasma-desktop kscreen konsole dolphin plasma-pa kde-gtk-config nvidia-settings firefox qtpass qjackctl sddm-kcm plasma-systemmonitor ksystemstats qt5-tools kate
 
 ## VM Packages
-sudo pacman -S libvirt edk2-ovmf virt-manager dnsmasq dmidecode qemu-full swtpm
+sudo pacman -S libvirt edk2-ovmf virt-manager dnsmasq dmidecode qemu-full swtpm virt-viewer
 
 sudo systemctl enable sddm
